89146bd5f8bba3d8aa2d9a5a9cfa699d054e02ea
Max Holder <mxhold@gmail.com>
2021-08-09 22:14:45 -0400

    Semi-animated walking

--- src/main.rs
+++ src/main.rs
@@ -31,54 +31,132 @@ fn main() {
 // }
 
 fn sprite_system(
-    mut query: Query<(&mut TextureAtlasSprite, &Direction)>,
+    time: Res<Time>,
+    mut query: Query<(&mut TextureAtlasSprite, &Direction, &mut WalkAnimation)>,
 ) {
-    for (mut sprite, direction) in query.iter_mut() {
-        match direction {
-            Direction::North => {
-                sprite.index = 1;
-            }
-            Direction::South => {
-                sprite.index = 7
-            }
-            Direction::East => {
-                sprite.index = 13;
-            }
-            Direction::West => {
-                sprite.index = 19;
-            }
+    for (mut sprite, direction, mut walk_animation) in query.iter_mut() {
+        sprite.index = walk_animation.stage.sprite_index() + direction.sprite_offset(6);
+
+        if walk_animation.stage == WalkStage::Stop {
+            continue;
+        }
+
+        walk_animation.timer.tick(time.delta());
+
+        if walk_animation.timer.finished() {
+            walk_animation.next();
         }
     }
 }
 
-fn keyboard_movement(keyboard_input: Res<Input<KeyCode>>, mut query: Query<(&Player, &mut Transform, &mut Direction)>) {
-    for (_, mut transform, mut direction) in query.iter_mut() {
+fn keyboard_movement(keyboard_input: Res<Input<KeyCode>>, mut query: Query<(&Player, &mut Transform, &mut Direction, &mut WalkAnimation)>) {
+    for (_, mut transform, mut direction, mut walk_animation) in query.iter_mut() {
         if keyboard_input.just_pressed(KeyCode::Right) {
             transform.translation.x += 20.;
             *direction = Direction::East;
+            *walk_animation = WalkAnimation::new(WalkStage::Step1)
         }
         if keyboard_input.just_pressed(KeyCode::Left) {
             transform.translation.x -= 20.;
             *direction = Direction::West;
+            *walk_animation = WalkAnimation::new(WalkStage::Step1)
         }
         if keyboard_input.just_pressed(KeyCode::Up) {
             transform.translation.y += 20.;
             *direction = Direction::North;
+            *walk_animation = WalkAnimation::new(WalkStage::Step1)
         }
         if keyboard_input.just_pressed(KeyCode::Down) {
             transform.translation.y -= 20.;
             *direction = Direction::South;
+            *walk_animation = WalkAnimation::new(WalkStage::Step1)
         }
     }
 }
 
 struct Player;
 
+#[derive(Eq, PartialEq, Copy, Clone)]
 enum Direction {
     North,
     South,
     East,
     West
+}
+
+impl Direction {
+    pub fn sprite_offset(&self, frames_per_direction: u32) -> u32 {
+        match self {
+            Direction::North => frames_per_direction * 0,
+            Direction::South => frames_per_direction * 1,
+            Direction::East => frames_per_direction * 2,
+            Direction::West => frames_per_direction * 3,
+        }
+    }
+}
+
+#[derive(PartialEq, Eq, Copy, Clone)]
+enum WalkStage {
+    Stop,
+    Step1,
+    Pause,
+    Step2
+}
+
+impl WalkStage {
+    pub fn next(&self) -> Self {
+        match self {
+            WalkStage::Step1 => WalkStage::Pause,
+            WalkStage::Pause => WalkStage::Step2,
+            WalkStage::Step2 => WalkStage::Stop,
+            WalkStage::Stop => WalkStage::Stop,
+        }
+    }
+
+    pub fn duration(&self) -> f32 {
+        match self {
+            WalkStage::Step1 => 0.2,
+            WalkStage::Pause => 0.2,
+            WalkStage::Step2 => 0.2,
+            WalkStage::Stop => 0.0,
+        }
+    }
+
+    pub fn sprite_index(&self) -> u32 {
+        match self {
+            WalkStage::Step1 => 0,
+            WalkStage::Pause => 1,
+            WalkStage::Step2 => 2,
+            WalkStage::Stop => 1,
+        }
+    }
+}
+
+struct WalkAnimation {
+    pub stage: WalkStage,
+    pub timer: Timer
+}
+
+impl WalkAnimation {
+    pub fn next(&mut self) {
+        self.stage = self.stage.next();
+        if self.stage != WalkStage::Stop {
+            self.timer = Timer::from_seconds(self.stage.duration(), false)
+        }
+    }
+
+    pub fn new(stage: WalkStage) -> Self {
+        Self {
+            stage: stage,
+            timer: Timer::from_seconds(stage.duration(), false)
+        }
+    }
+}
+
+impl Default for WalkAnimation {
+    fn default() -> Self {
+        WalkAnimation::new(WalkStage::Stop)
+    }
 }
 
 fn setup(
@@ -101,5 +179,5 @@ fn setup(
         })
         .insert(Player)
         .insert(Direction::South)
-        .insert(Timer::from_seconds(0.1, true));
-}
+        .insert(WalkAnimation::default());
+    }
