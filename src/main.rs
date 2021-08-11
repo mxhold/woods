40dcaa06e5ac098881689047c0b6c2937ada657a
Max Holder <mxhold@gmail.com>
2021-08-11 11:02:34 -0400

    Animate walking

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use std::time::Duration;
 use bevy::prelude::*;
+
+const TILE_SIZE: f32 = 20.0;
+const STEP_DIST: f32 = TILE_SIZE / 3.0;
+
+const WALK_DURATION: Duration = Duration::from_millis(300/3);
 
 fn main() {
     App::build()
@@ -12,29 +18,14 @@ fn main() {
     .add_startup_system(setup.system())
     .add_system(keyboard_movement.system())
     .add_system(sprite_system.system())
-    // .add_system(animate_sprite_system.system())
     .run();
 }
 
-// fn animate_sprite_system(
-//     time: Res<Time>,
-//     texture_atlases: Res<Assets<TextureAtlas>>,
-//     mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &Handle<TextureAtlas>)>,
-// ) {
-//     for (mut timer, mut sprite, texture_atlas_handle) in query.iter_mut() {
-//         timer.tick(time.delta());
-//         if timer.finished() {
-//             let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
-//             sprite.index = ((sprite.index as usize + 1) % texture_atlas.textures.len()) as u32;
-//         }
-//     }
-// }
-
 fn sprite_system(
     time: Res<Time>,
-    mut query: Query<(&mut TextureAtlasSprite, &Direction, &mut WalkAnimation)>,
+    mut query: Query<(&mut TextureAtlasSprite, &Direction, &mut WalkAnimation, &mut Transform)>,
 ) {
-    for (mut sprite, direction, mut walk_animation) in query.iter_mut() {
+    for (mut sprite, direction, mut walk_animation, mut transform) in query.iter_mut() {
         sprite.index = walk_animation.stage.sprite_index() + direction.sprite_offset(6);
 
         if walk_animation.stage == WalkStage::Stop {
@@ -44,6 +35,20 @@ fn sprite_system(
         walk_animation.timer.tick(time.delta());
 
         if walk_animation.timer.finished() {
+            match direction {
+                Direction::East => {
+                    transform.translation.x += STEP_DIST;
+                }
+                Direction::West => {
+                    transform.translation.x -= STEP_DIST;
+                }
+                Direction::North => {
+                    transform.translation.y += STEP_DIST;
+                }
+                Direction::South => {
+                    transform.translation.y -= STEP_DIST;
+                }
+            }
             walk_animation.next();
         }
     }
@@ -52,22 +57,22 @@ fn sprite_system(
 fn keyboard_movement(keyboard_input: Res<Input<KeyCode>>, mut query: Query<(&Player, &mut Transform, &mut Direction, &mut WalkAnimation)>) {
     for (_, mut transform, mut direction, mut walk_animation) in query.iter_mut() {
         if keyboard_input.just_pressed(KeyCode::Right) {
-            transform.translation.x += 20.;
+            transform.translation.x += STEP_DIST;
             *direction = Direction::East;
             *walk_animation = WalkAnimation::new(WalkStage::Step1)
         }
         if keyboard_input.just_pressed(KeyCode::Left) {
-            transform.translation.x -= 20.;
+            transform.translation.x -= STEP_DIST;
             *direction = Direction::West;
             *walk_animation = WalkAnimation::new(WalkStage::Step1)
         }
         if keyboard_input.just_pressed(KeyCode::Up) {
-            transform.translation.y += 20.;
+            transform.translation.y += STEP_DIST;
             *direction = Direction::North;
             *walk_animation = WalkAnimation::new(WalkStage::Step1)
         }
         if keyboard_input.just_pressed(KeyCode::Down) {
-            transform.translation.y -= 20.;
+            transform.translation.y -= STEP_DIST;
             *direction = Direction::South;
             *walk_animation = WalkAnimation::new(WalkStage::Step1)
         }
@@ -113,14 +118,6 @@ impl WalkStage {
         }
     }
 
-    pub fn duration(&self) -> f32 {
-        match self {
-            WalkStage::Step1 => 0.2,
-            WalkStage::Pause => 0.2,
-            WalkStage::Step2 => 0.2,
-            WalkStage::Stop => 0.0,
-        }
-    }
 
     pub fn sprite_index(&self) -> u32 {
         match self {
@@ -141,14 +138,14 @@ impl WalkAnimation {
     pub fn next(&mut self) {
         self.stage = self.stage.next();
         if self.stage != WalkStage::Stop {
-            self.timer = Timer::from_seconds(self.stage.duration(), false)
+            self.timer = Timer::new(WALK_DURATION, false)
         }
     }
 
     pub fn new(stage: WalkStage) -> Self {
         Self {
             stage: stage,
-            timer: Timer::from_seconds(stage.duration(), false)
+            timer: Timer::new(WALK_DURATION, false)
         }
     }
 }
