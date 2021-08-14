7bdbb7542b65770f18d8c2c774619bda3fd71a11
Max Holder <mxhold@gmail.com>
2021-08-14 13:19:04 -0400

    Add basic server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,131 @@
-use bevy::prelude::*;
+use std::{net::SocketAddr, time::Duration};
+
+use bevy::{app::ScheduleRunnerSettings, prelude::*};
+use bevy_networking_turbulence::{
+    ConnectionChannelsBuilder, MessageChannelMode, MessageChannelSettings, NetworkEvent,
+    NetworkResource, NetworkingPlugin, ReliableChannelSettings,
+};
+use serde::{Deserialize, Serialize};
+use simple_logger::SimpleLogger;
+
+const SERVER_PORT: u16 = 14192;
 
 fn main() {
+    SimpleLogger::new()
+        .init()
+        .expect("A logger was already initialized");
+
     App::build()
         .insert_resource(ScheduleRunnerSettings::run_loop(Duration::from_secs_f64(
             1.0 / 60.0,
         )))
         .add_plugins(MinimalPlugins)
+        .add_plugin(NetworkingPlugin::default())
         .add_startup_system(setup.system())
         .add_system_to_stage(CoreStage::PreUpdate, handle_messages_server.system())
-        .add_system_to_stage(CoreStage::PostUpdate, network_broadcast_system.system())
+        .add_startup_system(network_setup.system())
+        .add_system(handle_packets.system())
         .run();
+}
+
+fn setup(mut net: ResMut<NetworkResource>) {
+    let ip_address =
+        bevy_networking_turbulence::find_my_ip_address().expect("can't find ip address");
+    let socket_address = SocketAddr::new(ip_address, SERVER_PORT);
+    log::info!("Starting server");
+    net.listen(socket_address, None, None);
+}
+
+fn network_setup(mut net: ResMut<NetworkResource>) {
+    net.set_channels_builder(|builder: &mut ConnectionChannelsBuilder| {
+        builder
+            .register::<ClientMessage>(CLIENT_STATE_MESSAGE_SETTINGS)
+            .unwrap();
+        builder
+            .register::<ServerMessage>(GAME_STATE_MESSAGE_SETTINGS)
+            .unwrap();
+    });
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+enum ClientMessage {
+    Hello(String),
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+enum ServerMessage {
+    Welcome(String),
+}
+
+const CLIENT_STATE_MESSAGE_SETTINGS: MessageChannelSettings = MessageChannelSettings {
+    channel: 0,
+    channel_mode: MessageChannelMode::Reliable {
+        reliability_settings: ReliableChannelSettings {
+            bandwidth: 4096,
+            recv_window_size: 1024,
+            send_window_size: 1024,
+            burst_bandwidth: 1024,
+            init_send: 512,
+            wakeup_time: Duration::from_millis(100),
+            initial_rtt: Duration::from_millis(200),
+            max_rtt: Duration::from_secs(2),
+            rtt_update_factor: 0.1,
+            rtt_resend_factor: 1.5,
+        },
+        max_message_len: 1024,
+    },
+    message_buffer_size: 8,
+    packet_buffer_size: 8,
+};
+
+const GAME_STATE_MESSAGE_SETTINGS: MessageChannelSettings = MessageChannelSettings {
+    channel: 1,
+    channel_mode: MessageChannelMode::Unreliable,
+    message_buffer_size: 8,
+    packet_buffer_size: 8,
+};
+
+fn handle_packets(mut net: ResMut<NetworkResource>, mut network_events: EventReader<NetworkEvent>) {
+    for event in network_events.iter() {
+        match event {
+            NetworkEvent::Connected(handle) => match net.connections.get_mut(handle) {
+                Some(connection) => {
+                    match connection.remote_address() {
+                        Some(remote_address) => {
+                            log::debug!(
+                                "Incoming connection on [{}] from [{}]",
+                                handle,
+                                remote_address
+                            );
+                        }
+                        None => {
+                            log::debug!("Connected on [{}]", handle);
+                        }
+                    }
+                    net.send_message(*handle, ServerMessage::Welcome("welcome!".to_owned()))
+                        .expect("Message failed");
+                }
+                None => panic!("Got packet for non-existing connection [{}]", handle),
+            },
+            _ => {}
+        }
+    }
+}
+
+fn handle_messages_server(mut net: ResMut<NetworkResource>) {
+    for (handle, connection) in net.connections.iter_mut() {
+        let channels = connection.channels().unwrap();
+        while let Some(client_message) = channels.recv::<ClientMessage>() {
+            log::debug!(
+                "ClientMessage received on [{}]: {:?}",
+                handle,
+                client_message
+            );
+            match client_message {
+                ClientMessage::Hello(id) => {
+                    log::info!("Client [{}] connected on [{}]", id, handle);
+                }
+            }
+        }
+    }
 }
